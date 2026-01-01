05ae7e948e1da7ccaf49621646070907cf955bc5
Sachin Chandra <sachin62bpk@gmail.com>
2026-01-01 16:54:33 +0530

    Add Blynk integration for smart home control

--- 101-Smart-Home-Automation/source-code/smart_home.ino
+++ 101-Smart-Home-Automation/source-code/smart_home.ino
@@ -1,1 +1,30 @@
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>  // For ESP8266, change to <WiFi.h> for ESP32
+#include <BlynkSimpleEsp8266.h> // For ESP32: BlynkSimpleEsp32.h
 
+// Replace with your network credentials
+char ssid[] = "YOUR_WIFI_NAME";
+char pass[] = "YOUR_WIFI_PASSWORD";
+
+// Blynk Auth Token
+char auth[] = "YOUR_BLYNK_AUTH_TOKEN";
+
+// Relay pin
+const int relayPin = D1;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, LOW); // Ensure relay OFF at start
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop() {
+  Blynk.run();
+}
+
+// Blynk Button Widget will call this function
+BLYNK_WRITE(V1) {
+  int buttonState = param.asInt();
+  digitalWrite(relayPin, buttonState);
+}
